a5a1d7f0a138ce4b5386efa62adb15e591a5d64a
Mohit M <mohitcrox@gmail.com>
2017-06-09 18:02:24 +0530

    mc

--- src/bot.js
+++ src/bot.js
@@ -19,7 +19,7 @@ app.get('/send_message', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8000, function () {
+app.listen(process.env.PORT || 8000, function () {
   console.log('Example app listening on port 8000!')
 })
 
